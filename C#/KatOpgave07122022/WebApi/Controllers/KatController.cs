2ad91bd4337196f50f0c0d6a2355b57643c48a95
madsenanders <50098546+madsenanders@users.noreply.github.com>
2023-01-07 13:00:25 +0100

    Add files via upload

---
+++ C#/KatOpgave07122022/WebApi/Controllers/KatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class KatController : Controller
+    {
+        // GET: Kat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
